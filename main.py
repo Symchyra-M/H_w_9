6f65ccff6ad13d4f74dd139c4530acfa4f9b4def
Symchyra-M <137912223+Symchyra-M@users.noreply.github.com>
2023-09-27 16:45:21 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-contacts ={}
+contacts ={} # Словник для зберігання контактів
 
 #Обробка помилок введених користувачем
 def input_error(func):
@@ -14,45 +14,6 @@ def input_error(func):
 
     return inner
 
-
-
-#Функція для збереження у пам'яті (у словнику) новий контакт.
-@input_error
-def add_contact(user_input):
-    _, name, phone = user_input.split()
-    contacts[name] = phone
-    return f"Contact {name} added with phone {phone}"
-
-#Функція для збереження нового номера телефону до збереженого контакту.
-@input_error
-def change_number(user_input):
-    _, name, phone = user_input.split()
-    if name in contacts:
-        contacts[name] = phone
-        return f"Contact {name} changed number to{phone}"
-    else:
-        return f"Contact {name} not found"
-
-
-
-#Функція для виводу номера телефону для зазначеного контакту
-@input_error
-def get_contact(user_input):
-    _, name, phone = user_input.split()
-    if name in contacts:
-        return f"Phon number for {name} is {contacts[phone]}"
-    else:
-        return f"Contact {name} not found"
-
-#Функція для виводу віх збережених контактів
-@input_error
-def show_all_contacts(user_input):
-    if contacts:
-        return '\n'.join([f'{name}: {phone}' for name, phone in contacts.items()])
-    else:
-        return 'No contacts'
-
-
 @input_error
 def main():
     print("Список команд:\n "
@@ -64,9 +25,9 @@ def main():
           "'good bye','close','exit', 'stop'- завершення роботи")
 
     while True:
-        user_input = input(">>>").lower()
+        user_input = input("Enter a command: ").lower()
 
-        if user_input == 'good bye' or user_input == 'exit' or user_input == 'close' or user_input == 'stop':
+        if user_input in ['good bye', 'exit', 'close', 'stop']:
             print('Good bye!')
             break
 
@@ -75,23 +36,77 @@ def main():
 
         #За цією командою бот зберігає у пам'яті (у словнику) новий контакт.
         elif user_input.startswith('add'):
-           print(add_contact(user_input))
+            print(add_contact(user_input))
 
         #За цією командою бот зберігає в пам'яті новий номер телефону збереженого контакту.
         elif user_input.startswith('change'):
-           print(change_number(user_input))
+            print(change_number(user_input))
 
         #За цією командою бот виводить у консоль номер телефону для зазначеного контакту.
         elif user_input.startswith('phone'):
-           print(get_contact(user_input))
+            print(get_contact(user_input))
 
         #За цією командою бот виводить всі збереженні контакти з номерами телефонів у консоль.
         elif user_input == 'show all':
-           print(show_all_contacts(user_input))
+            print(show_all_contacts())
 
         else:
-           print('Invalid command, try again')
+            print('Invalid command, try again')
 
+
+
+#Функція для збереження у пам'яті (у словнику) новий контакт.
+@input_error
+def add_contact(data):
+    words = data.split(' ')
+    if len(words) == 3 :
+        contacts[words[1]] = words[2]
+        return f"Contact {words[1]} added with phone {words[2]}"
+    else:
+        raise ValueError
+
+
+#Функція для збереження нового номера телефону до збереженого контакту.
+@input_error
+def change_number(data):
+    if contacts:
+        words = data.split(' ')
+        if len(words) == 3:
+            if words[1] in contacts:
+                contacts[words[1]] = words[2]
+                return f"Contact {words[1]} changed number to {words[2]}"
+            else:
+                return f"Contact {words[1]} not found"
+        else:
+            raise ValueError
+    else:
+        return 'No contacts'
+
+
+
+# Функція для виводу номера телефону для зазначеного контакту
+@input_error
+def get_contact(data):
+    if contacts:
+        words = data.split(' ')
+        if len(words) == 2:
+            if words[1] in contacts:
+                return f"Phon number for {words[1]} is {contacts[words[1]]}"
+            else:
+                return f"Contact {words[1]} not found"
+        else:
+            raise KeyError
+    else:
+        return 'No contacts'
+
+
+#Функція для виводу віх збережених контактів
+@input_error
+def show_all_contacts():
+    if contacts:
+        return '\n'.join([f'{name}: {phone}' for name, phone in contacts.items()])
+    else:
+        return 'No contacts'
 
 
 
